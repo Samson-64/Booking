95240f44ba63fd41f027c6abd5354bf60c01a2f0
Samson-64 <148760141+Samson-64@users.noreply.github.com>
2026-09-01 13:01:27 +0300

    change the backend url import

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,22 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    proxy: {
-      '/api': { target: import.meta.env.VITE_API_BASE_URL, changeOrigin: true },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react(), tailwindcss()],
+
+    server: {
+      host: '0.0.0.0',
+      port: 3000,
+
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+        },
+      },
     },
-  },
+  }
 })
