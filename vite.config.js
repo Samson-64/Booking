562d3eceea4fbfa930f96a4158277a5db02a603d
Samson-64 <148760141+Samson-64@users.noreply.github.com>
2026-09-01 12:33:05 +0300

    change the backend link

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3000,
     proxy: {
-      '/api': { target: 'http://127.0.0.1:8000', changeOrigin: true },
+      '/api': { target: 'https://backend-fnks.onrender.com', changeOrigin: true },
     },
   },
 })
