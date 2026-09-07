84e8f0480b1f9ead73a61ccf1ce2ac02fec48780
Samson-64 <148760141+Samson-64@users.noreply.github.com>
2026-09-07 09:19:26 +0300

    change spinner color

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -4,7 +4,7 @@ function Spinner({ label = "Loading…" }) {
       <div className="relative flex h-9 w-9 items-center justify-center">
         <div className="absolute h-9 w-9 animate-ping rounded-full bg-teal-400/20" />
         <svg
-          className="h-7 w-7 animate-spin text-teal-600"
+          className="h-7 w-7 animate-spin text-slate-900"
           viewBox="0 0 24 24"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
